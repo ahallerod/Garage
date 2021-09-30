3c692cdadbf82ccc8973d6dab5d260f4c979ba7c
ahallerod <90375472+ahallerod@users.noreply.github.com>
2021-09-30 14:08:01 +0200

    Started UML

---
+++ Garage/Garage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Garage
+{
+    public class Garage
+    {
+        public IEnumerable ParkedVehicles
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public void ListVehicles()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
