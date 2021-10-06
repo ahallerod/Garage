6805cbdc7252b5490c2be0ce814a1da883770517
ahallerod <90375472+ahallerod@users.noreply.github.com>
2021-10-06 09:08:20 +0200

    file handling embryo

---
+++ Garage/FileHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace Garage
+{
+    class FileHandler
+    {
+        public static void SaveGarage(Garage garage)
+        {
+            string x = JsonSerializer.Serialize(garage.GetType());
+            Console.WriteLine(x);
+
+        } 
+    }
+}
