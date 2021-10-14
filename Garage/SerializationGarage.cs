e99555396c2b88431e8f5f433a84e3ea3c02e9d9
Fredrik Lam <fredrik.didi.lam@gmail.com>
2021-10-14 13:20:48 +0200

    New class SerializationGarage

---
+++ Garage/SerializationGarage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage
+{
+    public class SerializationGarage
+    {
+        
+        public int Size { get; set; }
+        public List<Vehicle> ParkedVehicles { get; set; }
+        public SerializationGarage()
+        {}
+        public SerializationGarage(int size, List<Vehicle> vehicles)
+        {
+            Size = size;
+            ParkedVehicles = vehicles;
+        }
+            
+
+    }
+}
